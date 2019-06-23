42575dbbc3b24ec444a23dcce47ab2cee597263b
Matheus Weber <matheus.weber@ibm.com>
2019-06-23 19:27:02 -0300

    Add folder`s structure and common features

---
+++ iMovie/Common/Style/UIColor+Ext.swift
@@ -0,0 +1,69 @@
+//
+//  Colors.swift
+//  NubankExercise
+//
+//  Created by Matheus Weber on 20/03/18.
+//  Copyright © 2018 Matheus Weber. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension UIColor {
+    
+    static var purple: UIColor {
+        return #colorLiteral(red: 0.431372549, green: 0.168627451, blue: 0.4666666667, alpha: 1)
+    }
+    
+    static var black: UIColor {
+        return #colorLiteral(red: 0.1333333333, green: 0.1333333333, blue: 0.1333333333, alpha: 1)
+    }
+    
+    static var hint: UIColor {
+        return #colorLiteral(red: 0.6, green: 0.6, blue: 0.6, alpha: 1)
+    }
+    
+    static var background: UIColor {
+        return #colorLiteral(red: 0.9921568627, green: 0.9921568627, blue: 0.9921568627, alpha: 1)
+    }
+    
+    static var green: UIColor {
+        return #colorLiteral(red: 0.2549019608, green: 0.4588235294, blue: 0.01960784314, alpha: 1)
+    }
+    
+    static var red: UIColor {
+        return #colorLiteral(red: 0.8352941176, green: 0.09019607843, blue: 0.1058823529, alpha: 1)
+    }
+    
+    static var closeGray: UIColor {
+        return #colorLiteral(red: 0.5019607843, green: 0.5058823529, blue: 0.568627451, alpha: 1)
+    }
+    
+    static var disabledGray: UIColor {
+        return #colorLiteral(red: 0.8, green: 0.8, blue: 0.8, alpha: 1)
+    }
+    
+    static var placeholderGray: UIColor {
+        return #colorLiteral(red: 0.8, green: 0.8, blue: 0.8, alpha: 1)
+    }
+    
+    static var borderGray: UIColor {
+        return #colorLiteral(red: 0.8, green: 0.8, blue: 0.8, alpha: 1)
+    }
+    
+    static var loadingBlack: UIColor {
+        return #colorLiteral(red: 0.2666666667, green: 0.2666666667, blue: 0.2666666667, alpha: 1)
+    }
+    
+    static var backgroundBackView: UIColor {
+        return #colorLiteral(red: 0.2666666667, green: 0.2666666667, blue: 0.2666666667, alpha: 1)
+    }
+    
+    static var activedButtonGray: UIColor {
+        return #colorLiteral(red: 0.5019607843, green: 0.5058823529, blue: 0.568627451, alpha: 1)
+    }
+    
+    static var disabledButtonGray: UIColor {
+        return #colorLiteral(red: 0.8, green: 0.8, blue: 0.8, alpha: 1)
+    }
+}
