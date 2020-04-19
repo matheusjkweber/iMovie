fa2fa566d976e819f6ef177e22aeddae3f5b84d7
Matheus Weber <matheusweber@MacBook-Pro-de-Matheus-4.local>
2020-04-19 12:21:22 -0300

    Done

---
+++ iMovie/API/API/Service/HTTPTask.swift
@@ -0,0 +1,29 @@
+//
+//  HTTPTask.swift
+//  NetworkLayer
+//
+//  Created by Matheus Weber on 30/03/18.
+//  Copyright © 2018 Matheus Weber. All rights reserved.
+//  This Network Layer was created from Malcolm Kumwenda`s Medium Tutorial
+//
+
+import Foundation
+
+public typealias HTTPHeaders = [String:String]
+
+public enum HTTPTask {
+    case request
+    
+    case requestParameters(bodyParameters: Parameters?,
+        bodyEncoding: ParameterEncoding,
+        urlParameters: Parameters?)
+    
+    case requestParametersAndHeaders(bodyParameters: Parameters?,
+        bodyEncoding: ParameterEncoding,
+        urlParameters: Parameters?,
+        additionHeaders: HTTPHeaders?)
+    
+    // case download, upload...etc
+}
+
+
